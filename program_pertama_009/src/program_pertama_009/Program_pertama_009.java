c243b96cb17600a7388d1324d0bf9b1d5f2893ee
Alfirasmanar <alfirassmanar11@gmail.com>
2021-09-07 15:55:29 +0700

    BP 12021
    BP 12021

---
+++ program_pertama_009/src/program_pertama_009/Program_pertama_009.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program_pertama_009;
+
+/**
+ *
+ * @author Al-Ikhlas
+ */
+import java.util.Scanner;
+
+public class Program_pertama_009 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    //      Variabelnya
+
+        String name,univ;
+
+        int age;
+
+        double tinggi;
+
+        
+
+//      Isi dari variabelnya
+
+        name = "Muhammad Ulin Nuha AlFiras Manar";
+
+        univ = "UPN Veteran Jatim";
+
+        age = 19;
+
+        tinggi = 166.3;
+
+        
+
+//      Outputnya
+
+        System.out.println("Nama : " + name);
+
+        System.out.println("Universitas : " + univ);
+
+        System.out.println("umur : " + age);
+
+        System.out.println("Tinggi : " + tinggi);
+
+
+
+    }
+
+    }
+   
+}
